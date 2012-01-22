d55433ec861b7ac4ecfe2eeb6e3baa4385e6ca62
Ryan Brodkin <ryan@glabstudios.com>
2012-01-21 19:22:12 -0800

    Added log controller for non-database event logging.

---
+++ application/controllers/log.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+/**
+ * Log Controller
+ *
+ * @package		CodeIgniter
+ * @subpackage	Rest Server
+ * @category	Controller
+ * @author		Ryan Brodkin
+*/
+
+class Log extends REST_Controller
+{
+	/**
+	 * Get Profile Base Data
+	 */
+	function event_post()
+	{
+		$q = $this->db	->set('event_type', $this->post('event_type'))
+						->set('ip_address', 'INET_ATON(\''.$this->post('ip_address').'\')',false);
+
+		if ($this->post('pid') == false)
+		{
+			$q->set('pid', $this->post('pid'));
+		}
+
+		if ($this->post('data') == false)
+		{
+			$q->set('data',$this->post('data'));
+		}
+
+		if ($q->insert('event_log'))
+		{
+			$this->response(null, 200);
+		}
+		else
+		{
+			$this->response(null, 500);
+		}
+	}
+}
