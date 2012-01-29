74b7339e74bf1e386bdb48df09a8eb59fbe3c668
Ryan Brodkin <ryan@glabstudios.com>
2012-01-29 13:27:11 -0800

    Adding prototype for RESTful controllers.

---
+++ application/controllers/prototype.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Controller_Name Controller
+ *
+ * @package		CodeIgniter
+ * @subpackage	Rest Server
+ * @category	Controller
+ * @author		Ryan Brodkin
+*/
+
+class Controller_Name extends REST_Controller
+{
+	function method_get()
+	{
+			$this->response(null, 200);
+	}
+}
